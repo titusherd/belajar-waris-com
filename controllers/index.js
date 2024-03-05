e2f6febbb3bf25626ef297645dd69e6570fc17bf
Titus Herdiawan <76503729+titusherd@users.noreply.github.com>
2024-03-05 19:11:18 +0700

    update

---
+++ controllers/index.js
@@ -0,0 +1,71 @@
+import 'dotenv/config'
+import { createRequire } from "module";
+import axios from "axios";
+
+
+const require = createRequire(import.meta.url);
+const midtrans = require('midtrans-client');
+
+class Controller {
+    static getData = (req, res) => {
+        // res.send('Hello, World!');
+        // console.log("EXPRESS");
+        const data = req.body;
+        console.log('Received post data:', data);
+        // console.log('Received post data: TESTTT');
+        // res.send('Testtt');
+
+        // Process the post data, save to database, etc.
+    }
+
+    static midtrans = (req, res) => {
+        const snap = new midtrans.Snap({
+            isProduction: false,
+            serverKey: process.env.SERVER_KEY,
+            clientKey: process.env.CLIENT_KEY
+        });
+
+        const user = req.body;
+
+
+        const parameter = {
+            "transaction_details": {
+                "order_id": "test-transaction-123" + Math.round((new Date()).getTime() / 1000),
+                "gross_amount": 200000
+            }, "credit_card": {
+                "secure": true
+            }
+        };
+
+        snap.createTransaction(parameter)
+            .then((transaction) => {
+                // transaction token
+                let transactionToken = transaction.token;
+                console.log('transactionToken:', transactionToken);
+                console.log(user);
+                // res.send(transactionToken);
+                res.json({ transactionToken: transactionToken, user });
+            })
+            .catch((error) => {
+                console.log('error:', error);
+            });
+    }
+
+    static zoho = (req, res) => {
+        const apiUrl = "https://flow.zoho.com/839171716/flow/webhook/incoming?zapikey=1001.27f0c2b0e2836c2dde1a6ea74f750e7d.af27c5777413cd30816dd86a7b79507c&isdebug=false";
+        const recivedData = req.body;
+        console.log('Received post data:', recivedData);
+
+        axios.post(apiUrl, recivedData)
+            .then(res => {
+                console.log('Response from external API:', res.data);
+                // res.json({ message: 'JSON data received and sent successfully' });
+            })
+            .catch(error => {
+                console.error('Error sending JSON data to external API:', error.message);
+                // res.status(500).json({ error: 'Error sending JSON data to external API' });
+            });
+    }
+}
+
+export default Controller;
---
+++ index.js
@@ -0,0 +1,28 @@
+import { createRequire } from "module";
+import Controller from "./controllers/index.js";
+
+const require = createRequire(import.meta.url);
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.post('/posts', Controller.midtrans);
+app.post('/zoho', Controller.zoho);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
+
+
+
+
