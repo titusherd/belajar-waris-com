3d326ad1478374a4031e71e0c003950006d08747
Titus Herdiawan <76503729+titusherd@users.noreply.github.com>
2024-03-08 23:51:00 +0700

    add redirect

--- src/views/Pendahuluan.vue
+++ src/views/Pendahuluan.vue
@@ -184,6 +184,7 @@ export default {
               button.disabled = true;
               button.className =
                 "rounded-md grey-400 px-3 py-2 text-sm font-semibold text-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600";
+              window.location.href = "https://www.belajarwaris.com";
             },
             onPending: (result) => {
               this.pendingAlert = true;
